a9edf859ed3b7061db452ce7dc313b32bf759c3d
WMSPanel <sales@wmspanel.com>
2013-02-25 09:09:42 +1100

    Initial versions of existing samples

---
+++ java/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) throws UnsupportedEncodingException {
+	    DateTimeFormatter timeFormatter = DateTimeFormat.forPattern("M/d/y h:m:s a").withZone(DateTimeZone.UTC).withLocale(Locale.US);
+		DateTime currentServerTime = new DateTime(DateTimeZone.UTC); // lets get localtime in UTC timezone
+		String today = timeFormatter.print(currentServerTime);
+		String initial_url = "rtsp://ec2-test-ip.compute.amazonaws.com:1935/live/Stream1";
+   	    String ip = "95.154.108.229";
+		String key = "defaultpassword";
+		String validminutes = "20";
+
+		String to_hash = ip + key + today + validminutes;
+		byte[] ascii_to_hash = to_hash.getBytes("UTF-8");
+		String base64hash = Base64.encodeBase64String(DigestUtils.md5(ascii_to_hash));
+		String urlsignature = "server_time=" + today  + "&hash_value=" + base64hash + "&validminutes=" + validminutes;
+		String base64urlsignature = Base64.encodeBase64String(urlsignature.getBytes("UTF-8"));
+		String signedurlwithvalidinterval = initial_url + "?wmsAuthSign=" + base64urlsignature;
+
+		return;
+	}
+}
---
+++ php/sample.php
@@ -0,0 +1,24 @@
+<?php
+
+$today = gmdate("n/j/Y g:i:s A");
+ 
+$initial_url = "rtsp://ec2-test-ip.compute.amazonaws.com:1935/live/Stream1";
+  
+$ip = $_SERVER['REMOTE_ADDR'];
+$key = "defaultpassword";
+ 
+$validminutes = 20;
+ 
+$str2hash = $ip . $key . $today . $validminutes;
+ 
+$md5raw = md5($str2hash, true);
+ 
+$base64hash = base64_encode($md5raw);
+ 
+$urlsignature = "server_time=" . $today ."&hash_value=" . $base64hash. "&validminutes=$validminutes";
+
+$base64urlsignature = base64_encode($urlsignature);
+
+$signedurlwithvalidinterval = $initial_url . "?wmsAuthSign=$base64urlsignature";
+ 
+?>

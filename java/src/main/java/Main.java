6e35f6fdf9ba6048c2a4daa0c910576456fb6550
Softvelum <sales@wmspanel.com>
2018-05-04 11:58:37 +1000

    Update Main.java

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -14,9 +14,8 @@ public class Main {
 	    DateTimeFormatter timeFormatter = DateTimeFormat.forPattern("M/d/y h:m:s a").withZone(DateTimeZone.UTC).withLocale(Locale.US);
 		DateTime currentServerTime = new DateTime(DateTimeZone.UTC); // lets get localtime in UTC timezone
 		String today = timeFormatter.print(currentServerTime);
-		String initial_url = "http://yourdomain.com:8081/live";
-		String video_url = "/Stream1";
-   	    String ip = "127.0.0.1";
+		String video_url = "http://yourdomain.com:8081/live/Stream1";
+   	        String ip = "127.0.0.1";
 		String key = "defaultpassword";
 		String validminutes = "20";
 
@@ -25,7 +24,7 @@ public class Main {
 		String base64hash = Base64.encodeBase64String(DigestUtils.md5(ascii_to_hash));
 		String urlsignature = "server_time=" + today  + "&hash_value=" + base64hash + "&validminutes=" + validminutes;
 		String base64urlsignature = Base64.encodeBase64String(urlsignature.getBytes("UTF-8"));
-		String signedurlwithvalidinterval = initial_url + "?wmsAuthSign=" + base64urlsignature + video_url;
+		String signedurlwithvalidinterval = video_url + "?wmsAuthSign=" + base64urlsignature;
 
 		return;
 	}

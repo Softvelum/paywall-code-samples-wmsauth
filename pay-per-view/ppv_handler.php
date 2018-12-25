8497f96ec70c0de9629c08fd6f975bffbccdc8b6
Softvelum <sales@wmspanel.com>
2018-12-25 15:04:57 +1000

    Update ppv_handler.php

--- pay-per-view/ppv_handler.php
+++ pay-per-view/ppv_handler.php
@@ -1,11 +1,12 @@
 <?php
 // Log entire incoming request to see what we have in it.
 $fp = fopen('/var/tmp/request.log', 'w');
-fwrite($fp, file_get_contents("php://input"));
+$post_data = file_get_contents("php://input");
+fwrite($fp, $post_data);
 fclose($fp);
 
 // Use this object for accessing each viewer's ID, IP and stream name.
-$sync_data = json_decode(file_get_contents("php://input"));
+$sync_data = json_decode($post_data);
 
 // Return IDs of clients which needs to be denied. Their IDs are 1 and 2 here.
 // Those viewers will be disconnected immediatelly and will not be allowed to connect anymore.
